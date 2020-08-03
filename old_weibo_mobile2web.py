da55d4cfb59cfa247be1f80300989c7feb458e3d
wowYou <32235270+wowYoungCoren@users.noreply.github.com>
2020-08-03 16:54:37 +0800

    Update old_weibo_mobile2web.py

--- old_weibo_mobile2web.py
+++ old_weibo_mobile2web.py
@@ -8,6 +8,7 @@ import pandas as pd
 import requests
 import re
 
+#用pandas包里专门读取excel下的各类文档类型的功能读取文档，并把‘链接’转为list数据类型
 df1=pd.read_excel("weibo(old).xlsx")
 urllist=df1.链接.tolist()
 
@@ -19,6 +20,8 @@ for i in urllist:
     bid=re.search(r'"bid": "([A-Za-z0-9]+)',res.text).group(1)
     url="https://weibo.com/"+uid+"/"+bid
     list.append(url)
+
     
+#输出保存为"hehe.csv"同时不带有列抬头
 pd.DataFrame(list).to_csv("hehe(old).csv",header=None,index=False)
 
