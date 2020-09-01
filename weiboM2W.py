a5405d815fc1015cba2b587f8e869244630f34e6
wowYoungCoren <xhjykk@gmail.com>
2020-09-01 16:40:16 +0800

    0901 移动注释位置

--- weiboM2W.py
+++ weiboM2W.py
@@ -28,34 +28,37 @@ def weiboM2W():
 
     # 历遍每一个网址
     for i in range(data.shape[0]):
-        if "://m.weibo" in data.iloc[i, 0]:  # 以“://m.weibo”判断是否为手机端
-
-            mobile_url = data.iloc[i, 0][-16:]  # 将每个手机端网址的后16位取出来
-
-            first = changeBase(int(mobile_url[-7:]), 62)  # 后七位进行转换
-
-            second = changeBase(int(mobile_url[-14:-7]), 62)  # 后八 -- 十四位进行转换
-
-            third = changeBase(int(mobile_url[:2]), 62)  # 前两位进行转换
-
-            if len(second) != 4:  # 微博机制：如果7位十进制数字转换后不满4个占位符，则需要在前面加一个0
+        # 以“://m.weibo”判断是否为手机端
+        if "://m.weibo" in data.iloc[i, 0]:
+            # 将每个手机端网址的后16位取出来
+            mobile_url = data.iloc[i, 0][-16:]
+            # 后七位进行转换
+            first = changeBase(int(mobile_url[-7:]), 62)
+            # 后八 -- 十四位进行转换
+            second = changeBase(int(mobile_url[-14:-7]), 62)
+            # 前两位进行转换
+            third = changeBase(int(mobile_url[:2]), 62)
+            # 微博机制：如果7位十进制数字转换后不满4个占位符，则需要在前面加一个0
+            if len(second) != 4:
                 second = "0" + second
 
             if len(first) != 4:
                 first = "0" + first
 
-            fix_url = third + second + first  # 转换后的62进制，位数必定是9位
-
-            data.iloc[i, 0] = data.iloc[i, 0][:30] + fix_url  # 将原有的10进制数字替换成62进制
-
-            data.iloc[i, 0] = data.iloc[i, 0].replace("m.", "")  # 规整格式
-
-            data.iloc[i, 0] = data.iloc[i, 0].replace("cn", "com")  # 规整格式
+            # 转换后的62进制，位数必定是9位
+            fix_url = third + second + first
+            # 将原有的10进制数字替换成62进制
+            data.iloc[i, 0] = data.iloc[i, 0][:30] + fix_url
+            # 规整格式
+            data.iloc[i, 0] = data.iloc[i, 0].replace("m.", "")
+            data.iloc[i, 0] = data.iloc[i, 0].replace("cn", "com")
         else:
             pass
-    data.columns = ['网页端url']  # 更换表头名
 
-    return data.to_excel(BASE_DIR + "网页端.xlsx", index=False)  # 输出excel
+    # 更换表头名
+    data.columns = ['网页端url']
+    # 输出excel
+    return data.to_excel(BASE_DIR + "网页端.xlsx", index=False)
 
 if __name__ == '__main__':
     weiboM2W()
