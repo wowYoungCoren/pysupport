eb5833731da035760cded4bcdfb15094bc665f1a
dennis <dennis33560@gmail.com>
2020-09-01 17:25:08 +0800

    试跑结果

--- weiboM2W.py
+++ weiboM2W.py
@@ -11,7 +11,7 @@ import tkinter as tk
 from tkinter import filedialog
 from readFile import *
 
-# hey man 2
+# hey man 21222
 # 62进制转10进制的函数
 def changeBase(n, b):
     baseList = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
